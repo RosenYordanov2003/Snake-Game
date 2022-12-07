751ef5bde80937798ce204fe702030799cb7dd6f
Rosen Yordanov <roskata182003@gmail.com>
2022-12-07 12:11:53 +0200

    Upload Snake Game Structure

---
+++ Snake/Snake/SimpleSnake/Core/Contracts/IEngine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSnake.Core.Contracts
+{
+    internal class IEngine
+    {
+    }
+}
---
+++ Snake/Snake/SimpleSnake/GameObjects/Foods/FoodHash.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleSnake.GameObjects.Foods
+{
+    public class FoodHash : Food
+    {
+        private const int DefaultPoints = 2;
+        private const char FoodSymbol = '#';
+        private const ConsoleColor DefaultColor = ConsoleColor.DarkMagenta;
+        public FoodHash(Wall wall) : base(wall, DefaultPoints, FoodSymbol, DefaultColor){}
+    }
+}
